735b4483bebaa9a88202ca91aea0b6d1987a9fa1
surajit29ghosh <surajit29ghosh@hotmail.com>
2021-06-19 22:12:39 +0530

    env updated

--- src/web/ecomApp/src/environments/environment.prod.ts
+++ src/web/ecomApp/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseURL: "http://api.app.internal"
+  pageSize: 50,
+  productApi: "http://localhost:7071/api/",
+  cartApi: "http://localhost:7072/api/"
 };
