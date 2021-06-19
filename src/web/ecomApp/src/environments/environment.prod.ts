7a391ab1ee63f81e3bc0a816dd99cc77edd22f79
surajit29ghosh <surajit29ghosh@hotmail.com>
2021-06-19 22:39:30 +0530

    updated api endpoints

--- src/web/ecomApp/src/environments/environment.prod.ts
+++ src/web/ecomApp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   pageSize: 50,
-  productApi: "http://localhost:7071/api/",
-  cartApi: "http://localhost:7072/api/"
+  productApi: "https://rm-shop-products-api.azurewebsites.net/api/",
+  cartApi: "https://rm-shop-cart-api.azurewebsites.net//api/"
 };
