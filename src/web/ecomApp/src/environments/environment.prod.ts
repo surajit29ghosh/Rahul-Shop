b01ac1ae1b5db5aa1d0b9eee59cf7d8b9f596698
surajit29ghosh <surajit29ghosh@hotmail.com>
2021-06-19 22:47:44 +0530

    fix cart api url

--- src/web/ecomApp/src/environments/environment.prod.ts
+++ src/web/ecomApp/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   pageSize: 50,
   productApi: "https://rm-shop-products-api.azurewebsites.net/api/",
-  cartApi: "https://rm-shop-cart-api.azurewebsites.net//api/"
+  cartApi: "https://rm-shop-cart-api.azurewebsites.net/api/"
 };
